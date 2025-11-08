952605f12e613ec73446fcf7ba31a9be02b55501
Narendran1424 <narendran1300@gmail.com>
2025-11-08 12:36:43 +0530

    Updated shared function

--- assets/js/blogs-details.js
+++ assets/js/blogs-details.js
@@ -3,13 +3,13 @@ const shareImgBtn=document.querySelector(".shareimage-btn");
 console.log(shareImgBtn)
 
 shareImgBtn.addEventListener("click",async()=>{
-    const imageUrl="https://narendran1424.github.io/quiet-climb/blogs-details.html/assets/images/blog-details1-img.png";
+    const imageUrl="https://narendran1424.github.io/quiet-climb/assets/images/blog-details1-img.png";
     
     try{
         const response=await fetch(imageUrl);
         const blob= await response.blob();
         console.log(blob.size)
-        const file=new File([blob],"https://narendran1424.github.io/quiet-climb/blogs-details.html/assets/images/blog-details1-img.png",{type:blob.type})
+        const file=new File([blob],"https://narendran1424.github.io/quiet-climb/assets/images/blog-details1-img.png",{type:blob.type})
         if(navigator.canShare({files:[file]})){
             await navigator.share({
                 title:'Loud Leaders',
