ce4271bdfe33552975510f0de06b2f04f4346a8a
Narendran1424 <narendran1300@gmail.com>
2025-11-08 20:30:58 +0530

    Added Share function

--- assets/js/blogs-details.js
+++ assets/js/blogs-details.js
@@ -1,26 +1,91 @@
-const shareImgBtn=document.querySelector(".shareimage-btn");
+const shareImgBtn1=document.querySelector(".blogs-content-section .blogs-section-container .date-share-icon  .shareimage-btn1");
 
-console.log(shareImgBtn)
+const shareImgBtn2=document.querySelector(".blogs-content-section .blogs-section-container .date-share-icon  .shareimage-btn2");
+const shareImgBtn3=document.querySelector(".blogs-content-section .blogs-section-container .date-share-icon  .shareimage-btn3");
 
-shareImgBtn.addEventListener("click",async()=>{
-    const imageUrl="https://narendran1424.github.io/quiet-climb/assets/images/blog-details1-img.png";
-    
-    try{
-        const response=await fetch(imageUrl);
-        const blob= await response.blob();
-        console.log(blob.size)
-        const file=new File([blob],"https://narendran1424.github.io/quiet-climb/assets/images/blog-details1-img.png",{type:blob.type})
-        if(navigator.canShare({files:[file]})){
-            await navigator.share({
-                title:'Loud Leaders',
-                text:'Loud Leaders',
-                files:[file]
-            });
-            console.log("image shared")
-        }else{
-            alert("your browser does not support")
-        }
-    }catch(error){
+const shareImgBtn4=document.querySelector(".blogs-content-section .blogs-section-container .date-share-icon  .shareimage-btn4");
+
+if(shareImgBtn1){
+    shareImgBtn1.addEventListener("click",async()=>{
+        const imageUrl="https://narendran1424.github.io/quiet-climb/assets/images/blog-details1-img.png";
+        
+        try{
+            const response=await fetch(imageUrl);
+            const blob= await response.blob();
+            const file=new File([blob],"https://narendran1424.github.io/quiet-climb/assets/images/blog-details1-img.png",{type:blob.type})
+            if(navigator.canShare({files:[file]})){
+                await navigator.share({
+                    title:'Loud Leaders',
+                    text:'Loud Leaders',
+                    files:[file]
+                });
+            }
+        }catch(error){
             console.error("Error while sharong this image")
         }
-})
+    })
+}
+
+
+if(shareImgBtn2){
+    shareImgBtn2.addEventListener("click",async()=>{
+        const imageUrl="https://narendran1424.github.io/quiet-climb/assets/images/blog-details2.png";
+        
+        try{
+            const response=await fetch(imageUrl);
+            const blob= await response.blob();
+            const file=new File([blob],"https://narendran1424.github.io/quiet-climb/assets/images/blog-details2.png",{type:blob.type})
+            if(navigator.canShare({files:[file]})){
+                await navigator.share({
+                    title:'Professionals At Work',
+                    text:'Professionals At Work',
+                    files:[file]
+                });
+            }
+        }catch(error){
+            console.error("Error while sharong this image")
+        }
+    })
+}
+
+if(shareImgBtn3){
+    shareImgBtn3.addEventListener("click",async()=>{
+        const imageUrl="https://narendran1424.github.io/quiet-climb/assets/images/blog-details3.png";
+        
+        try{
+            const response=await fetch(imageUrl);
+            const blob= await response.blob();
+            const file=new File([blob],"https://narendran1424.github.io/quiet-climb/assets/images/blog-details3.png",{type:blob.type})
+            if(navigator.canShare({files:[file]})){
+                await navigator.share({
+                    title:'Professionals At Work',
+                    text:'Professionals At Work',
+                    files:[file]
+                });
+            }
+        }catch(error){
+            console.error("Error while sharong this image")
+        }
+    })
+}
+
+if(shareImgBtn4){
+    shareImgBtn4.addEventListener("click",async()=>{
+        const imageUrl="https://narendran1424.github.io/quiet-climb/assets/images/blog-details4.png";
+        
+        try{
+            const response=await fetch(imageUrl);
+            const blob= await response.blob();
+            const file=new File([blob],"https://narendran1424.github.io/quiet-climb/assets/images/blog-details4.png",{type:blob.type})
+            if(navigator.canShare({files:[file]})){
+                await navigator.share({
+                    title:'Professionals At Work',
+                    text:'Professionals At Work',
+                    files:[file]
+                });
+            }
+        }catch(error){
+            console.error("Error while sharong this image")
+        }
+    })
+}
